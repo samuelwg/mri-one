88ed82b8d355e853e224f44b217362e40bcbd4ed
end.product <end.product@307e1836-5bde-88f9-4d86-52ea00cdd42d>
2011-08-22 22:50:01 +0000

    added noise filtering with average filter

--- NMR_main.m
+++ NMR_main.m
@@ -37,10 +37,10 @@ export_fig(h0, 'T1res.pdf')
 T2_star_list = dir ('T2_star*.csv');
 h2 = figure('PaperOrientation', 'landscape','position', pos, 'Color', 'white');
 for i=1:length(T2_star_list)
-    [T2_star_results(i,1), T2_star_results(i,2), T2_star_results(i,3), T2_star_results(i,4)] = T2_star(T2_star_list(i).name, i);
+    [T2_star_results(i,1), T2_star_results(i,2), T2_star_results(i,3), T2_star_results(i,4)] = T2_star(T2_star_list(i).name, i, 0);
 end
 
-%%% we are going to plot T_1 as function of concentration
+%%% we are going to plot T_2_star as function of concentration
 subplot (2,3,i+1)
 plot (T2_star_results(:,4), T2_star_results(:,2), 'o');
 title ('{T_2}^* as function of concentration of CuSo_4 ');
@@ -53,7 +53,7 @@ export_fig (h2, 'T2starres.pdf');
 T2_list = dir ('TT2_*CPMG.csv');
 h3 = figure('PaperOrientation', 'landscape','position', pos, 'Color', 'white');
 for i=1:length(T2_list)
-    [T2_results_CPMG(i,1), T2_results_CPMG(i,2), T2_results_CPMG(i,3), T2_results_CPMG(i,4)] = T2(T2_list(i).name, 1, i);
+    [T2_results_CPMG(i,1), T2_results_CPMG(i,2), T2_results_CPMG(i,3), T2_results_CPMG(i,4)] = T2(T2_list(i).name, 1, i, 0);
 end
 
 %% Now we'll plot T2 as a function of concentration
@@ -67,7 +67,7 @@ export_fig(h3, 'T2_CPMGres.pdf')
 T2_CP_list = dir ('TT2_*CP.csv');
 h4 = figure('PaperOrientation', 'landscape','position', pos, 'Color', 'white');
 for i=1:length(T2_CP_list)
-    [T2_results_CP(i,1), T2_results_CP(i,2), T2_results_CP(i,3), T2_results_CP(i,4)] = T2(T2_CP_list(i).name, 0, i);
+    [T2_results_CP(i,1), T2_results_CP(i,2), T2_results_CP(i,3), T2_results_CP(i,4)] = T2(T2_CP_list(i).name, 0, i, 0);
 end
 %% Now we'll plot T2 as a function of concentration
 subplot (2,3, i+1);
@@ -88,7 +88,23 @@ end
 T2_list = dir ('TT2_*CPMG.csv');
 h7 = figure('PaperOrientation', 'landscape','position', pos, 'Color', 'white');
 for i=1:length(T2_list)
-    [T2_results_CPMG(i,1), T2_results_CPMG(i,2), T2_results_CPMG(i,3), T2_results_CPMG(i,4)] = T2_filt(T2_list(i).name, 1, i, 7);
+    [T2_results_CPMG_f3(i,1), T2_results_CPMG_f3(i,2), T2_results_CPMG_f3(i,3), T2_results_CPMG_f3(i,4)] = T2_filt(T2_list(i).name, 1, i, 3);
+end
+
+%% Now we'll plot T2 as a function of concentration
+subplot (2,3, i+1);
+plot (T2_results_CPMG(:,4), T2_results_CPMG(:,2), '-o');
+title ('{T_2} (CPMG sequence) as function of concentration of CuSo_4 fitered 3p');
+xlabel('Concentration, perc');
+ylabel('{T_2}, [sec]');
+export_fig(h7, 'T2_CPMGres_filt3.pdf')
+
+
+
+T2_list = dir ('TT2_*CPMG.csv');
+h8 = figure('PaperOrientation', 'landscape','position', pos, 'Color', 'white');
+for i=1:length(T2_list)
+    [T2_results_CPMG_f5(i,1), T2_results_CPMG_f5(i,2), T2_results_CPMG_f5(i,3), T2_results_CPMG_f5(i,4)] = T2_filt(T2_list(i).name, 1, i, 5);
 end
 
 %% Now we'll plot T2 as a function of concentration
@@ -97,6 +113,44 @@ plot (T2_results_CPMG(:,4), T2_results_CPMG(:,2), '-o');
 title ('{T_2} (CPMG sequence) as function of concentration of CuSo_4 fitered 5p');
 xlabel('Concentration, perc');
 ylabel('{T_2}, [sec]');
-export_fig(h7, 'T2_CPMGres_filt.pdf')
+export_fig(h8, 'T2_CPMGres_filt5.pdf')
 
+h9 = figure('Color', 'white');
+plot(T2_results_CPMG(:,4), T2_results_CPMG(:,2), '-or');
+hold on;
+plot(T2_results_CPMG_f3(:,4), T2_results_CPMG_f3(:,2), '-ok');
+plot(T2_results_CPMG_f5(:,4), T2_results_CPMG_f5(:,2), '-ob');
+legend('not filtered data', 'moving average 3 points', 'moving average 5 points')
+title ('{T_2} (CPMG sequence) as function of concentration of CuSo_4 with various filter length');
+xlabel('Concentration, perc');
+ylabel('{T_2}, [sec]');
+export_fig(h9, 'T2_CPMGres_filt_diff.pdf')
+
+h10 = figure('Color', 'white');
+
+plot(T2_results_CPMG(:,4), T2_results_CPMG(:,3), '-or');
+hold on;
+plot(T2_results_CPMG_f3(:,4), T2_results_CPMG_f3(:,3), '-ok');
+plot(T2_results_CPMG_f5(:,4), T2_results_CPMG_f5(:,3), '-ob');
+legend('not filtered data', 'moving average 3 points', 'moving average 5 points', 'Location', 'SE')
+title ('{T_2} (CPMG sequence) fit goodness as function of concentration of CuSo_4 with various filter length');
+xlabel('Concentration, perc');
+ylabel('R^2');
+export_fig(h10, 'T2_CPMGres_filt_diff_Rsq.pdf')
+
+
+
+T2_star_list = dir ('T2_star*.csv');
+h11 = figure('PaperOrientation', 'landscape','position', pos, 'Color', 'white');
+for i=1:length(T2_star_list)
+    [T2_star_results(i,1), T2_star_results(i,2), T2_star_results(i,3), T2_star_results(i,4)] = T2_star(T2_star_list(i).name, i, 7);
+end
+
+%%% we are going to plot T_2_star as function of concentration
+subplot (2,3,i+1)
+plot (T2_star_results(:,4), T2_star_results(:,2), 'o');
+title ('{T_2}^* as function of concentration of CuSo_4 filtered 3p');
+xlabel('Concentration, perc');
+ylabel('{T_2}^*, [sec]');
+export_fig (h11, 'T2starres_f3.pdf');
 
